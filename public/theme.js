4f5be87dc5024e24801de21d6cba3c3df6cb57f9
Matheus <matheusferreirasjc@gmail.com>
2024-05-22 22:38:13 -0300

    v1.06 Adicionado toggle buttom para tema light/dark

--- public/theme.js
+++ public/theme.js
@@ -1,5 +1,22 @@
-document.getElementById('toggleTheme').addEventListener('click', function(){
-    document.body.classList.toggle('dark-theme');
+document.addEventListener('DOMContentLoaded', () => {
+    const toggle = document.getElementById('toggle');
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+    toggle.checked = localStorage.getItem('theme') === 'dark';
+
+    toggle.addEventListener('change', () => {
+        if (toggle.checked) {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('theme', 'light');
+        }
+    });
+
+    if (prefersDarkScheme.matches && !localStorage.getItem('theme')) {
+        document.documentElement.classList.add('dark');
+    } else if (localStorage.getItem('theme') === 'dark') {
+        document.documentElement.classList.add('dark');
+    }
 });
-
-
